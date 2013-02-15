a27d9ace0609d5abbc7780126f44504983fb2c5c
xaxes <xaxees@gmail.com>
2013-02-16 00:41:07 +0100

    Improved uptime; Downloaded and uploaded

--- stats.pl
+++ stats.pl
@@ -4,13 +4,22 @@ use Data::Dumper;
 use warnings;
 use strict;
 
+use Number::Bytes::Human qw(format_bytes);
+
 my $sleep = 5;
 while(){
-    my $uptime = `uptime`;
+    my $uptime = convert_time(uptime());
     my $uname = `uname -a`;
+
+    # Disk
     my $hdd = (split/\s+/,`df -h /media/hdd`)[11];
     my $hddfree = (split/\s+/,`df -h /media/hdd`)[10];
     my $hddpercent = (split/\s+/,`df -h /media/hdd`)[12];
+
+    # Network
+    my $rx = format_bytes(red("/sys/class/net/eth0/statistics/rx_bytes"));
+    my $tx = format_bytes(red("/sys/class/net/eth0/statistics/tx_bytes"));
+
 
     open FILE, ">/var/www/stats/index.html" or die $!;
 
@@ -28,7 +37,9 @@ while(){
             <div class="well">
                 <p><strong>Uptime:</strong> $uptime</p>
                 <p><strong>uname:</strong> $uname</p>
-                <p><strong>Free space on HDD:</strong> $hddfree/$hdd <div class="progress"><div class="bar" style="width: $hddpercent; text-indent: 50%; color: rgb(51, 51, 51);">$hddpercent</div></div></p>
+                <p><strong>Free space on HDD:</strong> $hddfree/$hdd <div class="progress"><div class="bar" style="width: $hddpercent; text-indent: 50%; color: rgb(51, 51, 51); ">$hddpercent</div></div></p>
+                <p><strong>Downloaded:</strong> $rx</p>
+                <p><strong>Uploaded:</strong> $tx</p>
             </div>
         <footer>
             <!--[if lte IE 8]><span style="filter: FlipH; -ms-filter: "FlipH"; display: inline-block;"><![endif]-->
@@ -45,4 +56,31 @@ HTML
 
     close FILE;
     sleep($sleep);
+}
+sub red{
+    my($k,$f)=(shift);
+    -f $k and open*h,"<".$k and sysread*h,$f,-s*h;
+    close*h;
+    return $f
+}
+sub uptime{
+    my$f=0;
+    open*h,"</proc/uptime" and sysread*h,$f,100;
+    close*h;
+    return $f=~/^(\d+)/
+}
+sub convert_time { 
+    my $time = shift; 
+    my $days = int($time / 86400); 
+    $time -= ($days * 86400); 
+    my $hours = int($time / 3600); 
+    $time -= ($hours * 3600); 
+    my $minutes = int($time / 60); 
+    my $seconds = $time % 60; 
+  
+    $days = $days < 1 ? '' : $days .'d '; 
+    $hours = $hours < 1 ? '' : $hours .'h '; 
+    $minutes = $minutes < 1 ? '' : $minutes . 'm '; 
+    $time = $days . $hours . $minutes . $seconds . 's'; 
+    return $time; 
 }
